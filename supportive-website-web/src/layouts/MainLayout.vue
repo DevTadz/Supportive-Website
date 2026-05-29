b8f4acd5365c5dbc23475eaeab6d446771758bca
Joakim Vogt Larsen <joaworkmail@gmail.com>
2026-05-29 20:35:17 +0200

    Added media player

--- supportive-website-web/src/layouts/MainLayout.vue
+++ supportive-website-web/src/layouts/MainLayout.vue
@@ -30,34 +30,71 @@
   </q-layout>
 
   <q-dialog v-model="mediaPlayer">
-    <q-card>
+    <q-card dark>
       <q-card-section>
+        <div style="width: 100%;" class="row justify-center">
+          <img src="../assets/nana_rose.jpg" style="height: 200px; width: auto;">
+        </div>
         <div class="text-h6">{{selectedSong?.title}}</div>
         <div>{{selectedSong?.author}}</div>
+
+            <q-linear-progress
+              :value="progress"
+              rounded
+              color="secondary"
+              style="background-color: white;"
+              class="q-mt-sm"
+            ></q-linear-progress>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <audio controls>
-          <source src="src\assets\music\nana.mp3" type="audio/mpeg">
-          Your browser does not support the audio element.
-        </audio>
-        <!-- <q-btn @click="Skip">Skip</q-btn> -->
+        <div class="row justify-between">
+          <q-btn icon="arrow_back_ios" class="bg-white mediaPLayerButton" rounded></q-btn>
+          <q-btn @click="togglePlay" :icon="playButtonicon" class="bg-white mediaPLayerButton" rounded></q-btn>
+          <q-btn icon="arrow_forward_ios" class="bg-white mediaPLayerButton" rounded></q-btn>
+        </div>
       </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn flat label="Close" color="primary" v-close-popup></q-btn>
+       
       </q-card-actions>
     </q-card>
   </q-dialog>
+  <audio ref="audioPlayer" :src="songFile" @timeupdate="updateProgress"></audio>
 </template>
 
+
 <script setup lang="ts">
-import { ref } from 'vue';
+import songFile from '../assets/music/nana.mp3'
+import { ref, onMounted, onBeforeUnmount} from 'vue';
 // import { useRoute } from 'vue-router';
 import { useRouter } from 'vue-router';
 
 const mediaPlayer = ref<boolean>(false)
 const router = useRouter();
+
+const audioPlayer = ref<HTMLAudioElement | null>(null)
+
+const progress = ref(0);
+
+const updateProgress = () => {
+  if (!audioPlayer.value) return
+
+  const current = audioPlayer.value.currentTime
+  const duration = audioPlayer.value.duration
+
+  if (duration > 0) {
+    progress.value = (current / duration)
+  }
+}
+
+onMounted(() => {
+  audioPlayer.value?.addEventListener('timeupdate', updateProgress)
+})
+
+onBeforeUnmount(() => {
+  audioPlayer.value?.removeEventListener('timeupdate', updateProgress)
+})
 
 interface Link {
   title: string;
@@ -92,8 +129,12 @@ const songList = ref<Song[]>([
 ]);
 
 const selectedSong = ref<Song | undefined>(songList.value[0]);
-
 const leftDrawerOpen = ref(false);
+
+const playIcon = ref<string>("play_arrow");
+const pauseIcon = ref<string>("pause");
+const playButtonicon = ref<string>("play_arrow");
+const isPlayingSong = ref<boolean>(false);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
@@ -101,6 +142,19 @@ function toggleLeftDrawer() {
 
 async function changePage(route: string) {
   await router.push(route);
+}
+
+async function togglePlay() {
+  if(!isPlayingSong.value) {
+    isPlayingSong.value = true;
+    playButtonicon.value = pauseIcon.value;
+    await audioPlayer.value?.play()
+  }
+  else {
+    isPlayingSong.value = false;
+    playButtonicon.value = playIcon.value;
+    audioPlayer.value?.pause()
+  }
 }
 
 // function Skip() {
@@ -132,5 +186,11 @@ async function changePage(route: string) {
 .menuButton {
   height: 50px;
   width: 100%;
+}
+
+.mediaPLayerButton {
+  color: black;
+  width: 40px;
+  height: 40px;
 }
 </style>
